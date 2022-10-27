f64d746e8f112df5f04a11b2f915a461d509fcaa
Andre Chen <andrec279@gmail.com>
2022-10-27 10:54:41 -0400

    added for package installation

--- env_setup.sh
+++ env_setup.sh
@@ -1,2 +1,2 @@
-pip install numpy pandas tqdm
-pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
+pip3 install numpy pandas tqdm
+pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
